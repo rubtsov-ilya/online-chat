ab5ae022147180ed98e1b67ef6799648eaeb580e
rubtsov-ilya <rubtsovilya398@gmail.com>
2024-10-15 16:31:55 +0300

    added attached-content-wrapper__item

--- src/components/pages/chat-page/chat-footer/attached-content-wrapper/AttachedContentWrapper.tsx
+++ src/components/pages/chat-page/chat-footer/attached-content-wrapper/AttachedContentWrapper.tsx
@@ -1,118 +1,48 @@
 import { FC } from 'react';
 import styles from './AttachedContentWrapper.module.scss';
-import second from 'src/assets/images/icons/24x24-icons/Close.svg?react'
+import CrossSvg from 'src/assets/images/icons/24x24-icons/Close.svg?react';
+import FileSvg from 'src/assets/images/icons/24x24-icons/File.svg?react';
 
 const AttachedContentWrapper: FC = () => {
   return (
     <div className={styles['attached-content-wrapper']}>
+
       <div className={styles['attached-content-wrapper__item']}>
         <img
-          className={styles['attached-content-wrapper__image']}
+          className={styles['attached-content-wrapper__media']}
           src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
         />
-        <button>
-
+        <button className={styles['attached-content-wrapper__cross-btn']}>
+          <CrossSvg
+            className={styles['attached-content-wrapper__cross-icon']}
+          />
         </button>
       </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/trees-growing-forest_1048944-30368869.jpg?w=1380"
-          alt=""
-        />
-      </div>
 
       <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/lake-with-flowers-mountain-background_1082141-16369.jpg?w=740"
-          alt=""
+        <video
+          className={styles['attached-content-wrapper__media']}
+          src="https://i.imgur.com/v5DeFBo.mp4"
         />
+        <button className={styles['attached-content-wrapper__cross-btn']}>
+          <CrossSvg
+            className={styles['attached-content-wrapper__cross-icon']}
+          />
+        </button>
       </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/lake-with-flowers-mountain-background_1082141-16369.jpg?w=740"
-          alt=""
-        />
-      </div>
-      <div className={styles['attached-content-wrapper__item']}>
-        <img
-          className={styles['attached-content-wrapper__image']}
-          src="https://img.freepik.com/premium-photo/lake-with-flowers-mountain-background_1082141-16369.jpg?w=740"
-          alt=""
-        />
+
+      <div
+        className={`${styles['attached-content-wrapper__item']} ${styles['attached-content-wrapper__item--file']}`}
+      >
+        <FileSvg className={styles['attached-content-wrapper__file-icon']} />
+        <span className={styles['attached-content-wrapper__file-name']}>
+          aasfasffsafaasf
+        </span>
+        <button className={styles['attached-content-wrapper__cross-btn']}>
+          <CrossSvg
+            className={`${styles['attached-content-wrapper__cross-icon']} ${styles['attached-content-wrapper__cross-icon--file']}`}
+          />
+        </button>
       </div>
     </div>
   );
